67da399ef52ca1adc9e3b4c87f396ae5a835be86
Andrew Gelstein <gelstein.andrew@gmail.com>
2022-08-07 16:32:18 -0500

    stubbing out page setup refactor

---
+++ src/Results.js
@@ -0,0 +1,5 @@
+const Results = () => {
+  return <div></div>;
+};
+
+export default Results;
---
+++ src/SearchParams.js
@@ -0,0 +1,5 @@
+const SearchParams = () => {
+  return <div></div>;
+};
+
+export default SearchParams;
