0d6f896051938f2250b44d16ee1510af5960263b
Andrew Gelstein <gelstein.andrew@gmail.com>
2022-08-09 16:00:36 -0500

    Words now fit on card. Fix search bar next.

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -27,14 +27,15 @@ const SearchParams = ({ token }) => {
       <form onSubmit={searchArtists}>
         <input type="text" onChange={(e) => setSearchKey(e.target.value)} />
         <button
-          className="inline-block ml-2 b-1 px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+          className="h-full inline-block ml-2 b-1 px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
           type={"submit"}
         >
           Search
         </button>
       </form>
-      <Results artists={artists} />
-      {/* <Artist artists={artists} /> */}
+      <div className="mt-4">
+        <Results artists={artists} />
+      </div>
     </div>
   );
 };
