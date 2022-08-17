ddf61a9ef4e90bb05b9beaae7d29c3e7ad7fdea0
Andrew Gelstein <gelstein.andrew@gmail.com>
2022-08-17 16:04:43 -0500

    some adjustments to styling on the page

--- src/Results.js
+++ src/Results.js
@@ -13,31 +13,39 @@ const Results = ({ hasBeenRun, artists, offset, searchArtists }) => {
   };
 
   return (
-    <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-      {!artists.length && hasBeenRun ? (
-        <h1>No Artists Found</h1>
-      ) : (
-        artists.map((artist) => {
-          return (
-            <Artist
-              key={artist.id}
-              images={artist.images}
-              name={artist.name}
-              id={artist.id}
-            />
-          );
-        })
-      )}
-      <div className="inline-flex">
+    <div>
+      <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+        {!artists.length && hasBeenRun ? (
+          <h1>No Artists Found</h1>
+        ) : (
+          artists.map((artist) => {
+            return (
+              <Artist
+                key={artist.id}
+                images={artist.images}
+                name={artist.name}
+                id={artist.id}
+              />
+            );
+          })
+        )}
+      </div>
+      <div className="flex space-x-4 justify-between mt-4">
         <button
           onClick={(e) => pageBack(e)}
-          className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l"
+          className=" bg-blue-600 text-white
+          font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700
+           hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+            active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out py-2 px-4 rounded-l"
         >
           Prev
         </button>
         <button
           onClick={(e) => pageFwd(e)}
-          className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r"
+          className=" bg-blue-600 text-white
+          font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700
+           hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+            active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out py-2 px-4 rounded-r"
         >
           Next
         </button>
