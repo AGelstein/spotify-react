7b861e408e1ce8acbd693c90dc71879a5e3267a3
Andrew Gelstein <gelstein.andrew@gmail.com>
2022-08-21 16:13:34 -0500

    extracted tailwind styling into template partial

--- src/Results.js
+++ src/Results.js
@@ -38,22 +38,10 @@ const Results = ({ hasBeenRun, artists, offset, searchArtists }) => {
             : "flex space-x-4 justify-between mt-4"
         }
       >
-        <button
-          onClick={(e) => pageBack(e)}
-          className=" bg-blue-600 text-white
-          font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700
-           hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
-            active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out py-2 px-4 rounded-l"
-        >
+        <button onClick={(e) => pageBack(e)} className="btn-primary">
           Prev
         </button>
-        <button
-          onClick={(e) => pageFwd(e)}
-          className=" bg-blue-600 text-white
-          font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700
-           hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
-            active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out py-2 px-4 rounded-r"
-        >
+        <button onClick={(e) => pageFwd(e)} className=" btn-primary">
           Next
         </button>
       </div>
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -40,13 +40,7 @@ const SearchParams = ({ token }) => {
           className="h-8 pt-1"
           onChange={(e) => setSearchKey(e.target.value)}
         />
-        <button
-          className="h-full inline-block ml-2 b-1 px-6 py-2.5 bg-blue-600 text-white
-           font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700
-            hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
-             active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-          type={"submit"}
-        >
+        <button className="btn-primary ml-2" type={"submit"}>
           Search
         </button>
       </form>
