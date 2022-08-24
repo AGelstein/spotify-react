9c52b62b1fa8435deeaa0cc1863a123e04c5e1b6
Andrew Gelstein <gelstein.andrew@gmail.com>
2022-08-23 19:17:23 -0500

    fixed odd login state

--- src/Header.js
+++ src/Header.js
@@ -15,7 +15,7 @@ const Header = ({ token, setToken }) => {
         Spotify React App
       </router-link>
       <div className="text-white px-3 rounded py-1">
-        {!token ? (
+        {!token && token !== 0 ? (
           <a
             href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
           >
