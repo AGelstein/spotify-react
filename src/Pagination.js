2d670184eacc702dc8edd6994af0e9b31301e295
Andrew Gelstein <gelstein.andrew@gmail.com>
2022-08-25 19:00:03 -0500

    starting pagination

---
+++ src/Pagination.js
@@ -0,0 +1,8 @@
+// import { useState } from "react";
+
+const Pagination = () => {
+  //   const [currentPage, setCurrentPage] = useState(1);
+  //   const [recordsPerPage] = useState(10);
+};
+
+export default Pagination;
