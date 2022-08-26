184f9bf55321482ff2ddb2821c88610f5e049c5e
Andrew Gelstein <gelstein.andrew@gmail.com>
2022-08-26 15:10:39 -0500

    still working this pagination out

--- src/Pagination.js
+++ src/Pagination.js
@@ -1,24 +1,28 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const Pagination = () => {
+const Pagination = (
+  nPages = { nPages },
+  currentPage = { currentPage },
+  setCurrentPage = { setCurrentPage }
+) => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
   const [recordsPerPage] = useState(10);
   const pageNumbers = [...Array(nPages + 1).keys()].slice(1);
 
-  useEffect(() => {
-    axios
-      .get("MOCK_DATA.json")
-      .then((res) => {
-        setData(res.data);
-        setLoading(false);
-      })
-      .catch(() => {
-        alert("There was an error while retrieving the data");
-      });
-  }, []);
+  // useEffect(() => {
+  //   axios
+  //     .get("MOCK_DATA.json")
+  //     .then((res) => {
+  //       setData(res.data);
+  //       setLoading(false);
+  //     })
+  //     .catch(() => {
+  //       alert("There was an error while retrieving the data");
+  //     });
+  // }, []);
 
   const nextPage = () => {
     if (currentPage !== nPages) setCurrentPage(currentPage + 1);
@@ -26,10 +30,6 @@ const Pagination = () => {
   const prevPage = () => {
     if (currentPage !== 1) setCurrentPage(currentPage - 1);
   };
-
-  const indexOfLastRecord = currentPage * recordsPerPage;
-  const indexOfFirstRecord = indexOfLastRecord - recordsPerPage;
-  const currentRecords = data.slice(indexOfFirstRecord, indexOfLastRecord);
 
   return <div></div>;
 };
