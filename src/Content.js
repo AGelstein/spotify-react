17a69ecdb2e29395f31075d6d89db58227f87f89
Andrew Gelstein <gelstein.andrew@gmail.com>
2022-09-05 16:33:51 -0500

    adjust padding

--- src/Content.js
+++ src/Content.js
@@ -2,7 +2,7 @@ import SearchParams from "./SearchParams";
 
 const Content = ({ token }) => {
   return (
-    <div className="p-20  bg-slate-200">
+    <div className="p-10  bg-slate-200">
       <SearchParams token={token}></SearchParams>
     </div>
   );
