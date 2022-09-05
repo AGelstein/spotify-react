5d7c4cbace8b5ff9b96a9a6d47b33aea245a7549
Andrew Gelstein <gelstein.andrew@gmail.com>
2022-09-05 11:09:27 -0500

    fixed the grid breaks for different screen sizes

--- src/Results.js
+++ src/Results.js
@@ -3,7 +3,7 @@ import Artist from "./Artist";
 const Results = ({ hasBeenRun, artists }) => {
   return (
     <div>
-      <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+      <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {!artists.length && hasBeenRun ? (
           <h1>No Artists Found</h1>
         ) : (
