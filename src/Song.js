a15b663a0c0af01494fde3bafd3a23602964e197
Andrew Gelstein <gelstein.andrew@gmail.com>
2022-10-12 15:30:35 -0500

    added new design for songs and grid to display said songs... do we need the grid?

---
+++ src/Song.js
@@ -0,0 +1,5 @@
+const Song = () => {
+  return <div className="relative block bg-white p-3 pb-6"></div>;
+};
+
+export default Song;
---
+++ src/SongGrid.js
@@ -0,0 +1,5 @@
+const SongGrid = () => {
+  return <div className="relative block bg-white p-3 pb-6"></div>;
+};
+
+export default SongGrid;
